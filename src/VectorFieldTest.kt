35f264ba7d8fee001603bfd132095535cefbf668
Luke <LukeLetourneau2000@gmail.com>
2019-04-09 21:09:29 -0400

    Updated test, removed more things that should be ignored

--- src/VectorFieldTest.kt
+++ src/VectorFieldTest.kt
@@ -1,6 +1,6 @@
 fun main() {
     val vectorField = VectorField(width = 1.0, height = 1.0, xComp = {x, y -> x}, yComp = {x, y -> y})
-    //vectorField.getFullField().forEach { println(it) }
+    vectorField.getFullField().forEach { println(it) }
 
     val xComp = {x: Double, y: Double -> x - y}
     val yComp = {x: Double, y: Double -> y - x}
